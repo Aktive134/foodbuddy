1c347e6d8734c565706781af4a8ad07a17d7aaad
Raymond Umukoro <reiymondjon134@gmail.com>
2023-04-30 18:36:05 +0100

    hero section updated

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -5,7 +5,15 @@ function Navbar() {
   return (
     <header className="header">
         <img src={images.foodbuddy} className="header__logo" alt="foodbuddy logo" />
-        <nav className="header__nav_main">Nav</nav>
+        <nav className="header__nav_main">
+            <ul className="header__nav_list">
+                <li><a className="header__nav_link" href="#">Section 1</a></li>
+                <li><a className="header__nav_link" href="#">Section 2</a></li>
+                <li><a className="header__nav_link" href="#">Section 3</a></li>
+                <li><a className="header__nav_link" href="#">Section 4</a></li>
+                <li><a className="header__nav_link nav_cta" href="#">Section 5</a></li>
+            </ul>
+        </nav>
     </header>
   )
 }
